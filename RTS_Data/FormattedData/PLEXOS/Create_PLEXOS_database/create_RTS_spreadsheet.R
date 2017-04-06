b93b7bfdca958582a4cdad9e9b7ca488f9129a5f
Barrows <clayton.barrows@nrel.gov>
2017-04-06 14:24:49 -0600

    fixing PLEXOS xml creation filepoitners

--- RTS_Data/FormattedData/PLEXOS/Create_PLEXOS_database/create_RTS_spreadsheet.R
+++ RTS_Data/FormattedData/PLEXOS/Create_PLEXOS_database/create_RTS_spreadsheet.R
@@ -1,7 +1,7 @@
 
 SourceData = normalizePath(file.path('../../../SourceData/'))
-
-source('1-parse-matpower/parse_matpower.R')
-setwd('../2-create-excel')
+output.dir = normalizePath(file.path('./1-parse-matpower/outputs/'))
+source('1-parse-matpower/parse_rts.R')
+setwd('2-create-excel')
 source('run_PSSE2PLEXOS.R')
 setwd('..')
