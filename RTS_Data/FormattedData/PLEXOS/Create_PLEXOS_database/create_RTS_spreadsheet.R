5421987a4dc496cec97fc44e38af90716dfcc7a5
cbarrows <cbarrows@nrel.gov>
2017-04-12 07:50:47 -0600

    adding package load to plexos creation script

--- RTS_Data/FormattedData/PLEXOS/Create_PLEXOS_database/create_RTS_spreadsheet.R
+++ RTS_Data/FormattedData/PLEXOS/Create_PLEXOS_database/create_RTS_spreadsheet.R
@@ -1,4 +1,4 @@
-
+pacman::p_load(data.table)
 SourceData = normalizePath(file.path('../../../SourceData/'))
 output.dir = normalizePath(file.path('./1-parse-matpower/outputs/'))
 source('1-parse-matpower/parse_rts.R')
