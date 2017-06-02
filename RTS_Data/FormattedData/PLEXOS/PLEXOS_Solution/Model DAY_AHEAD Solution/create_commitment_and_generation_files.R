801cf1095b362b6ed17ee0bbb34e1529ec1af0b9
Matt O'Connell <matthew.oconnell@nrel.gov>
2017-06-02 16:11:47 -0600

    adding commitment and generation scripts

---
+++ RTS_Data/FormattedData/PLEXOS/PLEXOS_Solution/Model DAY_AHEAD Solution/create_commitment_and_generation_files.R
@@ -0,0 +1,14 @@
+
+library(rplexos)
+library(reshape2)
+
+db = plexos_open(list.files(pattern = "\\.db$"))
+
+commitment = query_interval(db, 'Generator', 'Units Generating')
+generation = query_interval(db, 'Generator', 'Generation')
+
+commitment = dcast(commitment, time~name, value.var='value')
+generation = dcast(generation, time~name, value.var='value')
+
+write.csv(commitment, 'PLEXOS_DA_solution_commitment.csv', row.names = FALSE)
+write.csv(generation, 'PLEXOS_DA_solution_generation.csv', row.names = FALSE)
