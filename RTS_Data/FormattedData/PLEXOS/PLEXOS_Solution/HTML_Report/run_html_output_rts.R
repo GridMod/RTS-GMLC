b6322638f24e2b3d49dde488a511a1e0bfc2d75d
Matt O'Connell <matthew.oconnell@nrel.gov>
2017-06-05 15:15:41 -0600

    adding MAGMA files

---
+++ RTS_Data/FormattedData/PLEXOS/PLEXOS_Solution/HTML_Report/run_html_output_rts.R
@@ -0,0 +1,50 @@
+
+#------------------------------------------------------------------------------|
+# USER INPUT: set input parameters ----
+#------------------------------------------------------------------------------|
+setwd(dirname(parent.frame(2)$ofile))
+magma.dir        = 'MAGMA'
+input.csv        = '../input_data_rts.csv'
+db.loc           = '../../..'
+output.dir       = '../Reports'
+fig.path.name    = '../Plots'
+output.name      = 'RTS_html.html'
+db.day.ahead.loc = NULL
+query.data       = TRUE
+save.data        = FALSE
+load.data        = '<Name of file to load if query.data=FALSE >'
+save.data.name   = '<Name of file to save data. Will save in output.dir>'
+reassign.zones   = FALSE
+use.gen.type.csv = TRUE
+gen.type.csv.loc = '../gen_name_mapping_RTS.CSV'
+gen.region.zone  = '../gen_name_mapping_RTS.CSV'
+
+options(rplexos.tiebreak="first")
+
+#------------------------------------------------------------------------------|
+# Run code to create HTML
+#------------------------------------------------------------------------------|
+setwd(magma.dir)
+
+# Sourcing the setup file and required functions
+source(file.path('query_functions.R'))
+source(file.path('plot_functions.R'))
+
+# Read CSV file with all inputs
+inputs = read.csv(file.path(input.csv))
+inputs[inputs==""]=NA
+
+# Either query data from database or load existing data
+source(file.path('setup_plexosAnalysis.R'))
+if (query.data){
+  source(file.path('setup_dataQueries.R'), echo=TRUE)
+} else{
+  load(load.data)
+}
+
+render(input=file.path('HTML_output.Rmd'), c("html_document"), 
+       output_file=output.name, output_dir = file.path(output.dir,''))
+
+if (save.data){
+  save(list=ls(), file=file.path(output.dir,save.data.name))
+}
