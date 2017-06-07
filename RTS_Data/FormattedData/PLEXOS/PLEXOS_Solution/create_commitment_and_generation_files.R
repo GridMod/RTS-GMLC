3d654559e2eda6eda46c86af76f9091a32f512ac
McBennett <bmcbenne@nrel.gov>
2017-06-07 10:13:55 -0600

    add nodal price to PLEXOS-Solution folder

--- RTS_Data/FormattedData/PLEXOS/PLEXOS_Solution/create_commitment_and_generation_files.R
+++ RTS_Data/FormattedData/PLEXOS/PLEXOS_Solution/create_commitment_and_generation_files.R
@@ -8,18 +8,21 @@ if ( length(list.files(path = '../', pattern = c("DAY_AHEAD", "\\.db$")))>1 ) {
   message('More than one day ahead solution file found. Please include only the one to generate results for.')
 }
 
-db = plexos_open('..')
+db = plexos_open('../Model DAY_AHEAD Solution')
 db = db[grep('DAY_AHEAD', db$filename),]
 attributes(db)$class = c("rplexos","data.frame","tbl_df")
 
 commitment = query_interval(db, 'Generator', 'Units Generating')
 generation = query_interval(db, 'Generator', 'Generation')
+price = query_interval(db, 'Node', 'Price')
 
 commitment = dcast(commitment, time~name, value.var='value')
 generation = dcast(generation, time~name, value.var='value')
+price = dcast(price, time~name, value.var='value')
 
 write.csv(commitment, 'DAY_AHEAD Solution Files/PLEXOS_DA_solution_commitment.csv', row.names = FALSE)
 write.csv(generation, 'DAY_AHEAD Solution Files/PLEXOS_DA_solution_generation.csv', row.names = FALSE)
+write.csv(price, 'DAY_AHEAD Solution Files/PLEXOS_DA_solution_price.csv', row.names = FALSE)
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Real time run data querying
@@ -28,15 +31,18 @@ if ( length(list.files(path = '../', pattern = c("REAL_TIME", "\\.db$")))>1 ) {
   message('More than one real time solution file found. Please include only the one to generate results for.')
 }
 
-db = plexos_open('..')
+db = plexos_open('../Model REAL_TIME Solution')
 db = db[grep('REAL_TIME', db$filename),]
 attributes(db)$class = c("rplexos","data.frame","tbl_df")
 
 commitment = query_interval(db, 'Generator', 'Units Generating')
 generation = query_interval(db, 'Generator', 'Generation')
+price = query_interval(db, 'Node', 'Price')
 
 commitment = dcast(commitment, time~name, value.var='value')
 generation = dcast(generation, time~name, value.var='value')
+price = dcast(price, time~name, value.var='value')
 
 write.csv(commitment, 'REAL_TIME Solution Files/PLEXOS_RT_solution_commitment.csv', row.names = FALSE)
 write.csv(generation, 'REAL_TIME Solution Files/PLEXOS_RT_solution_generation.csv', row.names = FALSE)
+write.csv(price, 'REAL_TIME Solution Files/PLEXOS_RT_solution_price.csv', row.names = FALSE)
