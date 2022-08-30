f4c4ea878b0bb71d25a5ea9f508a634359c9ee5c
Erik Alvarez <erikfilias@gmail.com>
2022-08-30 12:28:51 +0200

    Update 1-parse-SourceData-to-Dictionaries.py

--- RTS_Data/FormattedData/openTEPES/Create_openTEPES_InputData/1-parse-SourceData-to-Dictionaries.py
+++ RTS_Data/FormattedData/openTEPES/Create_openTEPES_InputData/1-parse-SourceData-to-Dictionaries.py
@@ -3,5 +3,9 @@ import os
 import time
 import pandas as pd
 
-def GettingDictionaries(_path):
-    
+
+def GettingDictionaries(_path_data, _path_file):
+
+    # reading the bus information
+    df = pd.read_csv(_path_data+'/SourceData/bus.csv')
+
