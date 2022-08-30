6ff322aff32a24257350bf0610c492e986cb88b4
Erik Alvarez <erikfilias@gmail.com>
2022-08-30 12:28:48 +0200

    Update Create_openTEPES_RTS-GMLC.py

--- RTS_Data/FormattedData/openTEPES/Create_openTEPES_RTS-GMLC.py
+++ RTS_Data/FormattedData/openTEPES/Create_openTEPES_RTS-GMLC.py
@@ -2,5 +2,10 @@
 import os
 import pandas as pd
 
+import Create_openTEPES_InputData as ID
+
 # Setting up the path
-DIR    = os.path.dirname(__file__)
+_path_data = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..'))
+_path_file = os.path.dirname(__file__)
+
+ID.
