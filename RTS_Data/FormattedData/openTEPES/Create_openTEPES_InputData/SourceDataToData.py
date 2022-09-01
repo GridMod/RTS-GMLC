b36cc251384f23213748f7fa8e53e325fafcb149
Erik Alvarez <erikfilias@gmail.com>
2022-09-01 14:41:37 +0200

    Update SourceDataToData.py

--- RTS_Data/FormattedData/openTEPES/Create_openTEPES_InputData/SourceDataToData.py
+++ RTS_Data/FormattedData/openTEPES/Create_openTEPES_InputData/SourceDataToData.py
@@ -14,9 +14,12 @@ def GettingDataTo_oTData(_path_data, _path_file, CaseName):
 
     # reading data from the folder timeseries_data_file
     df_load    = pd.read_csv(_path_data + '/timeseries_data_files/Load/DAY_AHEAD_regional_Load.csv')
+    df_hydro   = pd.read_csv(_path_data + '/timeseries_data_files/Hydro/DAY_AHEAD_hydro.csv')
+    df_csp     = pd.read_csv(_path_data + '/timeseries_data_files/CSP/DAY_AHEAD_Natural_Inflow.csv')
 
     # reading data from the dictionaries
     df_Area          = pd.read_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Dict_Area_'      +CaseName+'.csv')
+    df_Gen           = pd.read_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Dict_Generation_'+CaseName+'.csv')
     df_Node          = pd.read_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Dict_Node_'      +CaseName+'.csv')
     df_NodeToZone    = pd.read_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Dict_NodeToZone_'+CaseName+'.csv')
     df_Period        = pd.read_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Dict_Period_'    +CaseName+'.csv')
@@ -27,11 +30,14 @@ def GettingDataTo_oTData(_path_data, _path_file, CaseName):
 
     #%% Defining the set 'node to area' and 'area to node'
     ar               = []
+    gen              = []
     nd               = []
     ndzn             = []
     znar             = []
     for i in df_Area.index:
         ar.append(df_Area['Area'][i])
+    for i in df_Gen.index:
+        gen.append(df_Gen['Generator'][i])
     for i in df_Node.index:
         nd.append(df_Node['Node'][i])
     for i in df_NodeToZone.index:
@@ -39,6 +45,7 @@ def GettingDataTo_oTData(_path_data, _path_file, CaseName):
     for i in df_ZoneToArea.index:
         znar.append((df_ZoneToArea['Zone'][i],df_ZoneToArea['Area'][i]))
     ar   = set(ar)
+    gen  = set(gen)
     nd   = set(nd)
     ndzn = set(ndzn)
     znar = set(znar)
@@ -55,7 +62,7 @@ def GettingDataTo_oTData(_path_data, _path_file, CaseName):
             ndar.append((i,j))
     ndar = set(ndar)
 
-    # Defining the nominal values of the areas
+    #%% Defining the nominal values of the demand per areas
     pNomDemand_org = df_bus[['Bus ID','Area','MW Load']]
     pNomDemArea    = pd.DataFrame(0, dtype=int, index=ar, columns=['MW'])
     for i in ar:
@@ -95,7 +102,48 @@ def GettingDataTo_oTData(_path_data, _path_file, CaseName):
     pDemand.to_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Data_Demand_'+CaseName+'.csv', sep=',', index=True)
 
     pDemand_File_Time    = time.time() - StartTime
-    StartTime = time.time()
+    StartTime            = time.time()
     print('pDemand file  generation               ... ', round(pDemand_File_Time), 's')
 
-    
+    #%% Getting the energy inflows
+    df_hydro['LoadLevel'] = pd.DataFrame({'LoadLevel': LoadLevels})
+    pHydroInflows         = df_hydro.iloc[: ,4 :]
+    pHydroInflows         = pHydroInflows.set_index(['LoadLevel'])
+    pHydroInflows         = pHydroInflows.replace(0, 0.0001)
+
+    df_csp['LoadLevel']   = pd.DataFrame({'LoadLevel': LoadLevels})
+    pCSPInflows           = df_csp.iloc[: ,4 :]
+    pCSPInflows           = pCSPInflows.set_index(['LoadLevel'])
+    pCSPInflows           = pCSPInflows.replace(0,0.0001)
+
+    pEnergyInflows        = pd.DataFrame(0, dtype=int, index=LoadLevels, columns=sorted(gen))
+
+    for i in pHydroInflows.columns:
+        pEnergyInflows.loc[:,i] = pHydroInflows.loc[:,i]
+
+    for i in pCSPInflows.columns:
+        pEnergyInflows.loc[:, i] = pCSPInflows.loc[:,i]
+
+    pEnergyInflows             = pEnergyInflows.reset_index()
+    pEnergyInflows['Period']   = df_Period.loc  [0, 'Period'  ]
+    pEnergyInflows['Scenario'] = df_Scenario.loc[0, 'Scenario']
+
+    pEnergyInflows = pEnergyInflows.set_index(['Period', 'Scenario', 'index'])
+    pEnergyInflows.to_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Data_EnergyInflows_'+CaseName+'.csv', sep=',', index=True)
+
+    pEnergyInflows_File_Time    = time.time() - StartTime
+    StartTime                   = time.time()
+    print('pEnergyInflows file  generation        ... ', round(pEnergyInflows_File_Time), 's')
+
+    #%% Getting the energy outflows
+    pEnergyOutflows             = pd.DataFrame(0, dtype=int, index=LoadLevels, columns=sorted(gen))
+    pEnergyOutflows             = pEnergyOutflows.reset_index()
+    pEnergyOutflows['Period']   = df_Period.loc  [0, 'Period'  ]
+    pEnergyOutflows['Scenario'] = df_Scenario.loc[0, 'Scenario']
+
+    pEnergyOutflows = pEnergyOutflows.set_index(['Period', 'Scenario', 'index'])
+    pEnergyOutflows.to_csv(_path_file+'/openTEPES_RTS-GMLC/oT_Data_EnergyOutflows_'+CaseName+'.csv', sep=',', index=True)
+
+    pEnergyOutflows_File_Time   = time.time() - StartTime
+    StartTime                   = time.time()
+    print('pEnergyOutflows file  generation       ... ', round(pEnergyOutflows_File_Time), 's')
